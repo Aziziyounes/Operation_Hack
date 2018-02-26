e4f5931e3e24a1011e9a65e3fb38b5ab2ec549e0
Juan M Chaves <chavesjuanm@gmail.com>
2018-02-25 20:09:21 -0800

    readme edit and website mock

---
+++ website/scripts/index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var x = document.getElementById("navDemo");
+    if (x.className.indexOf("show") == -1) {
+        x.className += " show";
+    } else { 
+        x.className = x.className.replace(" show", "");
+    }
+}
+
+console.log("JavaScript is amazing!");
